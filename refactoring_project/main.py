d638d250f77114a1fff4caf5070b3f4f252007cf
Moch. Chamdani Mustaqim <chamdani@outlook.com>
2022-01-30 03:52:56 +0700

    updated

--- refactoring_project/main.py
+++ refactoring_project/main.py
@@ -1,12 +1,47 @@
+"""
+Documentation
+This part is an application for deploying image classification using Deep Learning algorithm.
+Use this part as main service endpoint for managing raw data from frontend into service backend.
+It would provide a collection of data such (prediction class, metric performance etc) from service backend into frontend.
+"""
+
+# python package
 import os
 from flask import Flask, request, render_template
+
+# internal package
 from src import config
 
-app            = Flask(__name__)
-class_list     = {'GLIOMA': 0, 'MENINGIOMA': 1, 'PITUITARY': 2}
-path           = "static/model/"
-queryImagePath = "static/queryImage/"
+"""
+Local Config
+Comment this part before releasing your application in production
+"""
+app = Flask(__name__)
 
+"""
+Production Config
+uncomment and change the static_url_path to into url project path
+"""
+# app = Flask(__name__, static_url_path='/data_science_product/static')
+
+"""
+class_list is a global variable to define class dictionary 
+Keys as <class name> and Value as <class index>
+refer to class_indices for flow_from_directory function
+instead using dictionary u can use an ordred class list
+"""
+# class_list  = ['GLIOMA', 'MENINGIOMA', 'PITUITARY']
+class_dict          = {'GLIOMA': 0, 'MENINGIOMA': 1, 'PITUITARY': 2}
+# model_path is a global varible for model path
+model_path          = "static/model/"
+# query_image_path is a global varible for query image path
+query_image_path    = "static/queryImage/"
+
+"""
+IMPORTANT! 
+This part would handle after request cache while developing flask api
+It would clean and remove all development cache that could be happend
+"""
 @app.after_request
 def add_header(r):
     r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
@@ -15,10 +50,12 @@ def add_header(r):
     r.headers['Cache-Control'] = 'public, max-age=0'
     return r
 
+# ROUTING START
+# Render UI template for compare home page
 @app.route("/")
 def compare():
-    _, listModel, _             = config.GetDictModel(path)
-    imageClass, _, imageQuery   = config.GetListOfQueryImage(queryImagePath)
+    _, listModel, _             = config.GetDictModel(model_path)
+    imageClass, _, imageQuery   = config.GetListOfQueryImage(query_image_path)
     return render_template('/compare.html', listModel=listModel, imageQuery=imageQuery, imageClass=imageClass)
 
 @app.route('/pred_comp', methods=['POST'])
@@ -44,8 +81,8 @@ def predict_result_compare(model, runTime, result, image):
 
 @app.route('/select')
 def select():
-    _, listModel, _             = config.GetDictModel(path)
-    imageClass, _, imageQuery   = config.GetListOfQueryImage(queryImagePath)
+    _, listModel, _             = config.GetDictModel(model_path)
+    imageClass, _, imageQuery   = config.GetListOfQueryImage(query_image_path)
     return render_template('/select.html', listModel=listModel, imageQuery=imageQuery, imageClass=imageClass)
 
 @app.route('/pred_select', methods=['POST'])
