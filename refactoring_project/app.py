b06c748202294966febf7fb79c43808c65d41ff0
Moch. Chamdani Mustaqim <chamdani@outlook.com>
2022-02-01 02:27:11 +0700

    documentation done

--- refactoring_project/app.py
+++ refactoring_project/app.py
@@ -14,6 +14,25 @@ And you only need to change the app.py file as long as your project is Deeplearn
 You can change, add or remove any function each layer as you need (as long as it's not affect any service an attributes in this project)
 To get better understanding of the whole function that might be you need please kindly check and read the documentation first!
 
+TO CHANGE FILE
+
+Beside you change any line of code in this application layer, you were also need to change some files like models, weights, and query images sample
+
+You can find all models and weights in /static/model/ folder. Just delete any file in this folder and change with yours (models and weights *if any)
+Rename your models an weight by adding the current name with "_model" and "_weight" without "" after the current name 
+
+FOR EXAMPLE : 
+            * current name : VGG19.h5, VGG19.json, VGG19bobot.h5
+            * new name     : VGG19_model.h5, VGG19_model.json, VGG19bobot_weight.h5
+
+You can find all query images in /static/queryImage/ folder. Just delete any file in this folder and change with your query images sample
+Rename your query images by adding each current query image name with this pattern "<ClassName_><currentImageName>.<currentImageExtention>"
+Change the <ClassName_> without <> or ""
+
+FOR EXAMPLE : 
+            * current name : 410.jpg, 450.png, 110.jpeg
+            * new name     : Glioma_410.jpg, Meningioma_450.png, Pituitary_110.jpeg 
+
 @cham_is_fum
 """
 
